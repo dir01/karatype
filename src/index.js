dc1cbbb726b0b8921b20b661b0c17bbb4d41eddf
Andrew Zhu <dir01@dir01.org>
2016-12-22 23:06:02 +0300

    Little bag of style, gentlemen!

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 import './index.css';
 import Trial from './tutor/Trial';
 
-var trial = new Trial('Hello world');
+
+var trial = new Trial(
+    'The fundamental principle of science, the definition almost, is this: the sole test of the validity of any idea is experiment'
+);
+
 
 ReactDOM.render(
     <App trial={trial}/>,
