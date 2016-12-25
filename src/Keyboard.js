fbd0ec4cb8ead55ea8adc9ef13cc3076af2a2bf6
Andrew Zhu <dir01@dir01.org>
2016-12-25 03:25:48 +0300

    Keyboard component

---
+++ src/Keyboard.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Keyboard.css';
+import KeyboardLayout from './tutor/KeyboardLayout';
+
+
+class Keyboard extends Component {
+    render() {
+        let layout = new KeyboardLayout(this.props.layout);
+        return (
+            <div className="Keyboard">{
+                layout.rows.map((row, i) => {
+                    return <div className="Keyboard__Row" key={i}>
+                        {row.map(this.renderKey)}
+                    </div>;
+                })
+            }</div>
+        )
+    }
+
+    renderKey(key, i) {
+        return <div className={`KeyboardKey ${key.className}`} key={`${key.key}_${i}`}>{key.label || key.key}</div>;
+    }
+
+
+}
+
+
+class KeyboardKey extends Component {
+    render() {
+        return (
+            <div className={`KeyboardKey ${this.props.active ? 'active' : ''}`}>{this.props.label}</div>
+        )
+    }
+}
+
+let layouts = {
+    qwerty: [
+        '` 1 2 3 4 5 6 7 8 9 0 - = {backspace:label=←}',
+        '{tab} q w e r t y u i o p [ ] \\',
+        '{caps} a s d f g h j k l ; \' {enter}',
+        '{shift} z x c v b n m , . / {shift}',
+        '{empty} {empty} {empty} {space} {empty} {empty} {empty}'
+    ]
+};
+
+export { Keyboard, KeyboardKey, layouts };
---
+++ src/stories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { KeyboardKey, Keyboard, layouts } from '../Keyboard';
+
+
+storiesOf('KeyboardKey', module)
+    .add('normal', () => (
+        <KeyboardKey label='q'/>
+    ))
+    .add('active', () => (
+        <KeyboardKey label='q' active={true} />
+    ));
+
+storiesOf('Keyboard', module)
+    .add('qwerty', () => (
+        <Keyboard layout={layouts.qwerty} />
+    ));
