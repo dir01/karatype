20a217c6812e889e30022782eb5567d2be49f9ec
Andrew Zhu <dir01@dir01.org>
2017-02-26 00:42:35 +0300

    Tidy up html of Toolbar.button

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,9 +3,6 @@ import Indicator from '../Indicator/Indicator';
 import LevelSelector from '../LevelSelector/LevelSelector';
 import classNames from 'classnames';
 
-import restart from './svg/restart.svg';
-import pause from './svg/pause.svg';
-import skip from './svg/skip.svg';
 import './Toolbar.css';
 
 
@@ -13,32 +10,19 @@ export default class Toolbar extends Component {
     render() {
         return (
             <div className={ classNames('Toolbar', this.props.className) }>
-
-                <button className="Toolbar__button">
-                    <img src={ restart } role="presentation" />
-                </button>
-
-                <button className="Toolbar__button">
-                    <img src={ pause } role="presentation" />
-                </button>
-
-                <button className="Toolbar__button">
-                    <img src={ skip } role="presentation" />
-                </button>
-
+                <button className="Toolbar__button Toolbar__button--restart" />
+                <button className="Toolbar__button Toolbar__button--skip" />
                 <Indicator
                     className="Toolbar__Indicator"
                     text={ this.props.text }
                     progress={ this.props.progress }
                  />
-
                 <LevelSelector
                     className="Toolbar__LevelSelector"
                     levels={ this.props.levels }
                     currentLevel={ this.props.currentLevel }
                     onLevelChange={ this.props.onLevelChange }
                 />
-
             </div>
         );
     }
