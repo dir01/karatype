1bb8ad383ff2c7be81b4f6b12fff058f44463c21
Andrew Zhu <dir01@dir01.org>
2017-02-26 00:42:44 +0300

    Fix Indicator’s tests

--- src/components/Indicator/Indicator.test.js
+++ src/components/Indicator/Indicator.test.js
@@ -11,12 +11,12 @@ it('renders without crashing', () => {
 
 it('contains progressbar with progress=', () => {
     const wrapper = render(<Indicator progress="55" />);
-    expect(wrapper.find('.progressBar').length).to.equal(1);
-    expect(wrapper.find('#Indicator>span').length).to.equal(0);
+    expect(wrapper.find('.Indicator__progressbar').length).to.equal(1);
+    expect(wrapper.find('.Indicator>span').length).to.equal(0);
 });
 
 it('doesn\'t contain progressBar with text=', () => {
     const wrapper = render(<Indicator text="hello <b>world</b>" />);
-    expect(wrapper.find('.progressBar').length).to.equal(0);
-    expect(wrapper.find('#Indicator>span').length).to.equal(1);
+    expect(wrapper.find('.Indicator__progressbar').length).to.equal(0);
+    expect(wrapper.find('.Indicator>span').length).to.equal(1);
 });
